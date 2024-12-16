5fb339706ea87e58b92e7c55050661e6b0c0538c
uthaman0505 <uthaman_muthaiah@yahoo.com>
2024-12-16 14:37:37 +0800

    modified packages

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorCommunityBluetoothLe',
+      name: 'capacitorCapacitorBluetoothLeTektician',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
